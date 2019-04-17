fc3291469451a97c8f7128309c63e0ad8e7054dd
Md Kays <mdkayes00@gamil.com>
2019-04-17 21:41:55 +0600

    my cv

---
+++ skills.js
@@ -0,0 +1,51 @@
+var skills = [
+  {
+    technology: 'Html',
+    projects: '30+',
+    exeperiences: '2+',
+    rating: 8, 
+  },
+  {
+    technology: 'Css',
+    projects: '30+',
+    exeperiences: '2+',
+    rating: 8, 
+  },
+  {
+    technology: 'JS',
+    projects: '20+',
+    exeperiences: '2+',
+    rating: 8, 
+  },
+  {
+    technology: 'jQuery',
+    projects: '20+',
+    exeperiences: '2+',
+    rating: 8, 
+  },
+  {
+    technology: 'PHP',
+    projects: '10+',
+    exeperiences: '1+',
+    rating: 7, 
+  },
+  {
+    technology: 'MySql',
+    projects: '10+',
+    exeperiences: '1+',
+    rating: 7, 
+  },
+  {
+    technology: 'Laravel',
+    projects: '3',
+    exeperiences: '1+',
+    rating: 6, 
+  },
+  {
+    technology: 'WordPress',
+    projects: '10+',
+    exeperiences: '1+',
+    rating: 7, 
+  },
+ 
+]
